4d5ac5e8d819371696faffcbcb9cda70d01b6951
mirzzie <mirzad.official@outlook.com>
2025-09-13 21:55:46 +0100

    wrkflow fix 3 and rm scripts

--- scripts/run_zap.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-TARGET=$1  # e.g., http://34.12.34.56:3000
-docker run --rm -v $(pwd):/zap/wrk/:rw owasp/zap2docker-stable zap-baseline.py -t $TARGET -r zap-baseline-report.html -j zap-baseline-report.json
