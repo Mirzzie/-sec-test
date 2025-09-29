fb9ec1f4a753a5590d1d9b2715604891077c6f42
mirzzie <mirzad.official@outlook.com>
2025-09-29 10:52:51 +0100

    change ip

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -7,26 +7,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Fetch your current public IP
-data "http" "my_ip" {
-  url = "https://ifconfig.me/ip"
-}
-
-# Fetch GitHub meta JSON
-data "http" "github_meta" {
-  url = "https://api.github.com/meta"
-}
-
-# Decode JSON and extract Actions runner IPs
-locals {
-  github_actions_ipv4 = [
-    for cidr in jsondecode(data.http.github_meta.response_body).actions :
-    cidr
-    if can(regex("^\\d+\\.\\d+\\.\\d+\\.\\d+/", cidr))
-  ]
-}
-
-
 resource "aws_security_group" "staging_sg" {
   name        = "staging-sg"
   description = "Allow SSH and app port"
@@ -45,13 +25,7 @@ resource "aws_security_group" "staging_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # ingress {
-  #   description = "DVWA from GitHub Actions"
-  #   from_port   = 4280
-  #   to_port     = 4280
-  #   protocol    = "tcp"
-  #   cidr_blocks = local.github_actions_ipv4
-  # }
+
     ingress {
     from_port   = 3306
     to_port     = 3306
